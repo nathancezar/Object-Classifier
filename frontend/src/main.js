4447fb6708cd83947a76276dde784190f6c2583c
Nathan <nathancezar@gmail.com>
2021-08-29 21:51:50 -0300

    Alterado main.py

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import { BootstrapVue } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
+
+const backend = process.env.NODE_ENV === 'development' ? 'http://localhost:8888' : 'http://150.162.177.83:8888';
+
+Vue.mixin({
+    data() {
+        return {
+            get backend() {
+                return backend;
+            }
+        }
+    }
+});
+
 new Vue({
-  render: h => h(App),
+    components: { App },
+    router,
+    render: h => h(App),
 }).$mount('#app')
