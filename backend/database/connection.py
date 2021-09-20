ec46a2c2f1e101246640699a93268c9b4b37a521
Nathan <nathancezar@gmail.com>
2021-09-20 10:17:21 -0300

    Alterado nome do projeto. Nome do BD passado para .env.

--- backend/database/connection.py
+++ backend/database/connection.py
@@ -3,12 +3,13 @@ import os
 from peewee import PostgresqlDatabase, Model
 
 
+_database = os.environ.get("DATABASE_URL")
 _user = os.environ.get("DB_USER")
 _password = os.environ.get("DB_PASSWORD")
 _host = os.environ.get("DB_HOST")
 _port = os.environ.get("DB_PORT")
 
-database = PostgresqlDatabase("db_obj-classify",
+database = PostgresqlDatabase(  _database,
                                 user=_user,
                                 password=_password,
                                 host=_host,
