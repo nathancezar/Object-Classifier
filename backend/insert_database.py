2714cdb225a1b0a49df7b9de8fd0bb4e7601c7d1
Nathan Cezar Cardoso <nathancezar@gmail.com>
2021-10-22 17:58:11 -0300

    Ajustado script para inserir pastas no banco.

--- backend/insert_database.py
+++ backend/insert_database.py
@@ -7,28 +7,28 @@ from database.controllers.images_controller import ImagesController
 def insertDatabase(args):
     root = args["path"]
     rootFolders = tuple(map(lambda x: os.path.join(root, x), os.listdir(root)))
-    for dirname in rootFolders:
-        folders = os.listdir(dirname)
+    folders_paths = tuple(x for x in rootFolders if os.path.isdir(x))
+    folders_names = tuple(x for x in os.listdir(root) if os.path.isdir(os.path.join(root, x)))
+    
+    folderController = FoldersController()
+    imageController = ImagesController()
 
-        folderController = FoldersController()
-        imageController = ImagesController()
+    for name, path in zip(folders_names, folders_paths):
+        folder = folderController.getByName(name)
+        if folder:
+            continue
+        folderDict = {
+            "name": name,
+            "path": path,
+            "user_id": None
+        }
+        fId = folderController.insert(folderDict)
 
-        for f in folders:
-            folder = folderController.getByName(f)
-            if folder:
-                continue
-            folderDict = {
-                "name": f,
-                "path": os.path.join(dirname, f),
-                "user_id": None
-            }
-            fId = folderController.insert(folderDict)
-
-            images = os.listdir(os.path.join(dirname, f))
-            images = list(map(lambda img: {"name": img,
-                    "path": os.path.join(dirname, f, img),
-                    "folder": fId, "processed":False}, images))
-            imageController.insertMany(images)
+        images = [img for img in os.listdir(path) if os.path.isfile(img) and img.endswith('jpg')]
+        images = list(map(lambda img: {"name": img,
+                "path": os.path.join(path, img),
+                "folder": fId, "processed":False}, images))
+        imageController.insertMany(images)
 
 
 if __name__ == "__main__":
